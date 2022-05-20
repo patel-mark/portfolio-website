bc42571965ae2bf22d7dfdb2a9f7bce75ed4af18
mark patel <pmark2465@gmail.com>
2022-05-20 08:43:15 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const toggle=document.querySelector('.i');
+const list=document.getElementsByTagName('.ul');
+
+navbar__toggle.addEventListener('click',function(){
+    if (ul.style.display=='none') {
+        ul.style.display=='block';
+    } else {
+        ul.style.display=='none';
+    }
+});
